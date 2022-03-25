a7b9d2da1084f14ddf9cfd11af2723dae3ecea2c
Nayel Hamani <nhamani@FRL2310.local>
2022-03-25 20:59:38 +0100

    compute result.json

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from utils.utils import read_csv, lower_df, format_date, join_drugs_and_title
+import json
+
+import numpy as np
+
+from utils.utils import read_csv, lower_df, format_date, join_drugs_and_title, delete_chars, read_json_to_df
 import pandas as pd
 
 
@@ -7,43 +11,89 @@ def main():
 
 
 def start_pipeline():
-    drugs_df, pubmed_df, clinical_trials_df = get_csv()
-    drugs_df, pubmed_df, clinical_trials_df = process_csv(drugs_df, pubmed_df, clinical_trials_df)
-    joins(drugs_df, pubmed_df, clinical_trials_df)
+    drugs_df, pubmed_df, clinical_trials_df = get_files()
+    drugs_df, pubmed_df, clinical_trials_df = process_df(drugs_df, pubmed_df, clinical_trials_df)
+    nested_df = aggregate_df(drugs_df, pubmed_df, clinical_trials_df)
+    result_dict = generate_dict(nested_df)
+    write_to_json(result_dict)
 
 
-def get_csv():
+def get_files():
     clinical_trials_df = read_csv("ressources/clinical_trials.csv")
     drugs_df = read_csv("ressources/drugs.csv")
-    pubmed_df = read_csv("ressources/pubmed.csv")
+    pubmed_df1 = read_csv("ressources/pubmed.csv")
+    pubmed_df2 = read_json_to_df("ressources/pubmed.json")
+    pubmed_df = pd.concat([pubmed_df1,pubmed_df2]).reset_index(drop=True)
     return drugs_df, pubmed_df, clinical_trials_df
 
 
-def process_csv(drugs_df, pubmed_df, clinical_trials_df):
-    #lower str column
+def process_df(drugs_df, pubmed_df, clinical_trials_df):
+    # lower str column
     drugs_df = lower_df(drugs_df)
     pubmed_df = lower_df(pubmed_df)
     clinical_trials_df = lower_df(clinical_trials_df)
 
-    #format date
+    # clean characters
+    clinical_trials_df = delete_chars(clinical_trials_df)
+
+    # format date
     pubmed_df = format_date(pubmed_df, "date")
     clinical_trials_df = format_date(clinical_trials_df, "date")
+
+    # create and rename columns
+    pubmed_df["article_type"] = "pubmed"
+    clinical_trials_df["article_type"] = "clinical_trials"
+    clinical_trials_df = clinical_trials_df.rename(columns={"scientific_title": "title", "date": "date_mention"})
+    pubmed_df = pubmed_df.rename(columns={"date": "date_mention"})
+
     return drugs_df, pubmed_df, clinical_trials_df
 
 
-def joins(drugs_df, pubmed_df, clinical_trials_df):
-    pubmed_mention_df = join_drugs_and_title(drugs_df, pubmed_df, "title")
-    clinical_mention_df = join_drugs_and_title(drugs_df, clinical_trials_df, "scientific_title")
-    print(pubmed_mention_df)
-    print(clinical_mention_df)
+def aggregate_df(drugs_df, pubmed_df, clinical_trials_df):
+    pubmed_mention_df = join_drugs_and_title(drugs_df, pubmed_df)
+    clinical_mention_df = join_drugs_and_title(drugs_df, clinical_trials_df)
+    clinical_and_pubmed_mention_df = pd.concat([pubmed_mention_df, clinical_mention_df]).reset_index(drop=True)
+    journal_mention_df = clinical_and_pubmed_mention_df[
+        ["atccode", "drug", "date_mention", "journal"]].drop_duplicates().reset_index(
+        drop=True)
+    all_mention_df = pd.concat([pubmed_mention_df, clinical_mention_df, journal_mention_df]).reset_index(drop=True)
+    nested_df = all_mention_df.set_index(["atccode", 'drug'])[
+        ["date_mention", "journal", "title", "article_type"]].sort_index()
 
-    all_mentions_df = pd.concat([pubmed_mention_df, clinical_mention_df]).reset_index(drop=True)
-    print(all_mentions_df)
-    print(all_mentions_df.columns)
-    journal_mention_df = all_mentions_df[["drug", "date", "journal"]].drop_duplicates().reset_index(drop=True)
-    print(journal_mention_df)
+    return nested_df
 
 
+def generate_dict(nested_df):
+    unique_index = nested_df.index.drop_duplicates()
+    result_dict = []
+    for index_number, index_name in zip(range(len(unique_index)), unique_index):
+        intermediate_df = nested_df.loc[index_name]
+        result_dict.append([{
+            'atccode': index_name[0],
+            'drug': index_name[1],
+            'mention': [],
+        }])
+        for index, row in intermediate_df.iterrows():
+            mention_temp_dict = {
+                "date_mention": row['date_mention'].strftime("%Y/%m/%d"),
+                "article_type": row["article_type"] if row['title'] is not np.NaN else "journal",
+                "title": row['title'],
+                "journal": row['journal']}
+            result_dict[index_number][0]["mention"].append(mention_temp_dict)
+
+    return result_dict
+
+
+def write_to_json(result_dict):
+    with open('result.json', 'w') as json_file:
+        json.dump(result_dict, json_file, ensure_ascii=False)
+
+
+def journal_with_max_distinct_drugs():
+    with open("result.json") as data_file:
+        data = json.load(data_file)
+    print(data)
 
 if __name__ == "__main__":
+    #journal_with_max_distinct_drugs()
     main()
--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,21 @@
-from datetime import datetime
+import json
+from jsoncomment import JsonComment
 
 import pandas as pd
 
 
 def read_csv(filepath):
-    return pd.read_csv(filepath, encoding='utf-8')
+    return pd.read_csv(filepath)
 
+def read_json_to_df(filepath):
+    with open(filepath) as data_file:
+        parser = JsonComment(json) #handle trailing comma in json
+        data = parser.load(data_file)
+    return pd.DataFrame(data)
+
+def delete_chars(df):
+    df = df.applymap(lambda s: s.replace("\\xc3\\xb1", "") if type(s) == str else s)
+    return df.applymap(lambda s: s.replace("\\xc3\\x28", "") if type(s) == str else s)
 
 def lower_df(df):
     return df.applymap(lambda s: s.lower() if type(s) == str else s)
@@ -16,9 +26,9 @@ def format_date(df, column):
     return df
 
 
-def join_drugs_and_title(drugs_df, title_df, title_column):
+def join_drugs_and_title(drugs_df, title_df):
     # https://stackoverflow.com/questions/51955386/pandas-dataframe-merge-based-on-str-contains
     r = '({})'.format('|'.join(drugs_df.drug))
-    merge_df = title_df[title_column].str.extract(r, expand=False).fillna(title_df[title_column])
+    merge_df = title_df.title.str.extract(r, expand=False).fillna(title_df.title)
     title_mention = drugs_df.merge(title_df, left_on='drug', right_on=merge_df, how='inner')
     return title_mention
